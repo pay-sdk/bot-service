c82b6b99a2026bb46276716e995446ce2261d7c3
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-11-15 07:07:44 +0300

    Add new bot commands

--- src/main/java/org/paysdk/bot/BotApplication.java
+++ src/main/java/org/paysdk/bot/BotApplication.java
@@ -3,6 +3,7 @@ package org.paysdk.bot;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -25,12 +26,14 @@ public class BotApplication implements ApplicationRunner {
             updates.forEach(System.out::println);
 
             updates.forEach(update -> {
+
                 if (update.message().text().equals("/start")) {
                     bot.execute(new SendMessage(update.message().chat().id(),
                             "Добро пожаловать, " + update.message().from().firstName() + "!"));
                 } else {
                     processAddCommand(bot, update);
                     processHistoryCommand(bot, update);
+                    processNewCommand(bot, update);
                 }
             });
 
@@ -45,20 +48,33 @@ public class BotApplication implements ApplicationRunner {
         if (update.message().text().equals("/add")) {
             bot.execute(new SendMessage(update.message().chat().id(),
                     "Для того, чтобы зарегистрировать нового разработчика, отправьте " +
-                            "следующее сообщение с тремя элементами, " +
-                            "1.Команда new, 2.MerchantId и 3.SecretKey.\n" +
-                            "Обратите внимание, каждый элемент должен начинаться " +
-                            "с новой строки. Пример:\n\n" +
-                            "new\n384823903\n49308232"));
+                            "следующее сообщение с тремя элементами:\n\n" +
+                            "<b>1.Команда new</b>\n<b>2.MerchantId</b>\n<b>3.SecretKey</b>\n\n" +
+                            "<i>Обратите внимание, что каждый элемент должен начинаться " +
+                            "с новой строки. Пример:</i>\n\n" +
+                            "new\n384823903\n49308232").parseMode(ParseMode.HTML));
         }
     }
 
     private void processHistoryCommand(TelegramBot bot, Update update) {
         if (update.message().text().equals("/history")) {
             bot.execute(new SendMessage(update.message().chat().id(),
-                    "Платежи за последний месяц:\n" +
-                            "...\n" +
-                            "..."));
+                    "<b>Платежи за последний месяц</b>\n" +
+                            "... ...\n" +
+                            "... ...\n" +
+                            "... ...\n" +
+                            "... ...").parseMode(ParseMode.HTML));
+        }
+    }
+
+    private void processNewCommand(TelegramBot bot, Update update) {
+        // new message text
+        String message = update.message().text();
+
+        if (message.startsWith("new\n")) {
+            bot.execute(new SendMessage(update.message().chat().id(),
+                    "Ваш токен:\n\n" +
+                            "<b>KLjdi89REb3894Fdbb8KJEosfd3f3Ie4</b>").parseMode(ParseMode.HTML));
         }
     }
 }
